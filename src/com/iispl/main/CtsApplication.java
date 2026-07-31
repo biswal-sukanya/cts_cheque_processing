f26d5463b31e5cf5ffe22881baf2a9a1302b6de2
iispl <iispl@iispl>
2026-07-31 12:25:28 +0530

    created project repository template

--- src/com/iispl/main/CtsApplication.java
+++ src/com/iispl/main/CtsApplication.java
@@ -1,5 +1,14 @@
 package com.iispl.main;
 
-public class CtsApplication {
+import com.iispl.service.ChequeService;
+import com.iispl.service.ChequeServiceImpl;
 
+public class CtsApplication {
+	
+	static ChequeService chequeService = new ChequeServiceImpl();
+	
+public static void main(String[] args) {
+	
+	
+}
 }
