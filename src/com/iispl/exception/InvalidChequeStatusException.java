68ba2e11dc51197b55eb83dbe0cce9296879c681
SHAIR YAAR KHAN <shairyaark932@gmail.com>
2026-08-02 13:18:42 +0530

    cheque priority and status mandatory conditions

--- src/com/iispl/exception/InvalidChequeStatusException.java
+++ src/com/iispl/exception/InvalidChequeStatusException.java
@@ -4,6 +4,6 @@ public class InvalidChequeStatusException extends Exception {
 
 	@Override
 	public String getMessage() {
-		return "Invalid Cheque Status";
+		return "\nInvalid Cheque Status : Cheque Status is mandatory cannot be null";
 	}
 }
